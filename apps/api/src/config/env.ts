da7ae0bc018cf045f0a91c9ef5b198339f26ee77
kalachuuchiiii <parissrowlet@gmail.com>
2026-05-13 23:14:31 +0800

    remove prod env vars in env.ts config

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -30,12 +30,7 @@ const env = {
   DB_PORT: Number(getEnvVariable('DB_PORT')),
   DB_USER: getEnvVariable('DB_USER') as string,
   DB_HOST: getEnvVariable('DB_HOST') as string,
-  PROD_DB_HOST: getEnvVariable('PROD_DB_HOST') as string,
-  PROD_DB_PASS: getEnvVariable('PROD_DB_PASS') as string,
-  PROD_DB_USER: getEnvVariable('PROD_DB_USER') as string,
-  PROD_DB_NAME: getEnvVariable('PROD_DB_NAME') as string,
-  PROD_DB_PORT: Number(getEnvVariable('PROD_DB_PORT')) as number,
-  NODE_ENV: getEnvVariable('NODE_ENV') as
+  NODE_ENV: getEnvVariable('NODE_ENV', { required: false }) as
     | 'production'
     | 'development'
     | 'testing',
