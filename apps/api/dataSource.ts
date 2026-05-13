34580cceb7a498f3cfaf7b55a708c7a9edb12e82
kalachuuchiiii <parissrowlet@gmail.com>
2026-05-13 22:36:21 +0800

    remove node_env conditionals associated with db

--- apps/api/dataSource.ts
+++ apps/api/dataSource.ts
@@ -3,14 +3,15 @@ import { DataSource } from 'typeorm';
 import 'dotenv/config';
 
 const isProd = process.env.NODE_ENV === 'production';
+
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: (isProd ? process.env.PROD_DB_HOST : process.env.DB_HOST) as string,
+  host: process.env.DB_HOST as string,
   schema: 'public',
-  port: isProd ? Number(process.env.PROD_DB_PORT) : Number(process.env.DB_PORT),
-  username: (isProd ? process.env.PROD_DB_USER : process.env.DB_USER) as string,
-  password: (isProd ? process.env.PROD_DB_PASS : process.env.DB_PASS) as string,
-  database: (isProd ? process.env.PROD_DB_NAME : process.env.DB_NAME) as string,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER as string,
+  password: process.env.DB_PASS as string,
+  database: process.env.DB_NAME as string,
   migrations: ['dist/src/migrations/*.js'],
   entities: [`dist/src/modules/**/entities/*.entity.js`],
   synchronize: false,
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,15 +5,13 @@ import cors from 'cors';
 import AppDataSource from '../dataSource';
 import { env } from 'process';
 
-const isProd = env.NODE_ENV === 'production';
-
 async function bootstrap() {
   await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.use(
     cors({
-      origin: isProd ? env.CORS_ORIGIN : 'http://localhost:5173',
+      origin: env.CORS_ORIGIN ?? 'http://localhost:5173',
       credentials: true,
     }),
   );
